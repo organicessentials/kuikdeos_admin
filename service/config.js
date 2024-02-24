13fdc90e6f5593e6439a307e905bb39a62e48e35
sumitjambharkar <sujam3545@gmail.com>
2024-02-24 12:07:18 +0530

    commit

--- service/config.js
+++ service/config.js
@@ -1,8 +1,8 @@
 const config = {
-baseURL: 'http://localhost:3009/api', 
-baseApiURL: 'http://localhost:3009/',
-//baseURL: 'https://backend.organicessentialshub.com/api', // Replace with your API server URL
-//baseApiURL: 'https://backend.organicessentialshub.com/', // Replace with your API server URL
+//baseURL: 'http://localhost:3009/api', 
+//baseApiURL: 'http://localhost:3009/',
+baseURL: 'https://backend.kuikdose.io/api', // Replace with your API server URL
+baseApiURL: 'https://backend.kuikdose.io', // Replace with your API server URL
 
 };
 
